c23d4facbce9c39d03c3e183ea409226680980a7
Johnny Le <johnnyleespamacc@outlook.com>
2024-03-14 22:25:07 -0700

    upload for project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-c022cef8-c65c-463a-b672-e329f99ea992.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-8aeb8b7b-5c7e-4c0f-b0e2-efc6d4686f9d.containerhub.tripleten-services.com"
 }
